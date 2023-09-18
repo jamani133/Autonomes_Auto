9d319a762832bde5174281f6b3285e7ece491c83
jamani133 <68377235+jamani133@users.noreply.github.com>
2023-09-18 07:49:42 +0200

    ps9dufhsoduf
    i made oushzdfisugzdf

--- MotorController/MotorController.ino
+++ MotorController/MotorController.ino
@@ -6,3 +6,13 @@ void setup(){
 void loop(){
     
 }
+
+boolean procStep(int steps,int step){
+    int stackA = int(steps+1 / 2);
+    int stackB = int(steps / 2);
+    if(step >= 29){
+        return step%30 < stackA;
+    }else{
+        return step%30 < stackB;
+    }
+}
