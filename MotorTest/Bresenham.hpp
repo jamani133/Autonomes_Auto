6bb0acc25ae90c6c5b8b15b551c2b21d69fca71b
jamani133 <68377235+jamani133@users.noreply.github.com>
2023-10-16 13:41:08 +0200

    killll meeeeee

--- MotorTest/Bresenham.hpp
+++ MotorTest/Bresenham.hpp
@@ -3,12 +3,12 @@ class Bresenham{
     boolean queueStep = false;
     float steps = 3200.0;
 
-    float Ksteps_per_us = steps/1.0;
+    float us_per_step = steps/1.0;
     int cur_us = 0;
     int cur_steps = 0;
 
     void config(int steps_num = 0.0, int time = 1000000.0){
-        Ksteps_per_us = float(1000*steps_num)/float(time);
+        us_per_step = float(time)/float(steps_num);
         cur_us = 0;
         cur_steps = 0;
         queueStep = false;
@@ -16,7 +16,7 @@ class Bresenham{
 
     void advance(int adv){
         cur_us += adv;
-        float cur_y = (float(cur_us)*Ksteps_per_us)/1000;
+        float cur_y = cur_us/us_per_step;
         if(cur_y - float(cur_steps) > 0.5){
             cur_steps++;
             queueStep = true;
