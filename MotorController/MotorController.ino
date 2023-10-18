0431e88e3405bdd99792d703b0bade4f9136757a
jamani133 <68377235+jamani133@users.noreply.github.com>
2023-10-18 12:29:23 +0200

    testing wire

--- MotorController/MotorController.ino
+++ MotorController/MotorController.ino
@@ -7,9 +7,7 @@
 
 // C    D
 
-int FWD[]  = { 1, 1, 1, 1};
-int SIDE[] = {-1, 1, 1,-1};
-int ROT[]  = { 1,-1, 1,-1};
+
 
 boolean stopped = true;
 
--- MotorTest/MotorTest.ino
+++ MotorTest/MotorTest.ino
@@ -1,77 +1,63 @@
 #include "defMap.hpp"
 //#include "speedRamp.ino"
+#include "Wire.h"
 
-int FWD[]  = { 10, 10, 10, 10};
-
-int EXP[]  = { 3, 1, 7, 10};
-
-int SIDE[] = {-10, 10, -10,10};
-int ROT[]  = { 10,-10, -10,10};
-
-
-
+int FWD[]  = { 1, 1, 1, 1};
+int SIDE[] = {-1, 1, 1,-1};
+int ROT[]  = { 1,-1, 1,-1};
 //VL VR HR HL
 
 int wheelFreq[] = {0,0,0,0};
-
+int times[] = {0,0,0,0};
 //int curSteps[] = {0,0,0,0};
-
+bool running = false;
 
 
 void setup(){
-    
+    Wire.begin(0x01);
     configurePins();
     digitalWrite(MOTOR_ENABLE,true);
     delay(5000);
 
     digitalWrite(MOTOR_ENABLE,false);
-
+    Wire.onReceive(onWire);
 }
+
+//mm/1ksteps : 63.81171875
 void loop(){
-    const int dur = 3000;
-    const int velo = 200; 
-    digitalWrite(MOTOR_ENABLE,false);
-    manualAll(velo,EXP,dur);
-    manualAll(-velo,EXP,dur);
-    digitalWrite(MOTOR_ENABLE,true);
-    delay(3000);
-    digitalWrite(MOTOR_ENABLE,false);
-    manualAll(velo,FWD,dur);
-    manualAll(-velo,FWD,dur);
-    digitalWrite(MOTOR_ENABLE,true);
-    delay(3000);
-    digitalWrite(MOTOR_ENABLE,false);
-    manualAll(velo,SIDE,dur);
-    manualAll(-velo,SIDE,dur);
-    digitalWrite(MOTOR_ENABLE,true);
-    delay(3000);
-    digitalWrite(MOTOR_ENABLE,false);
-    manualAll(velo,ROT,dur);
-    manualAll(-velo,ROT,dur);
-    digitalWrite(MOTOR_ENABLE,true);
-    delay(3000);
-
-}
-
-
-void manualAll(int velo, int dirmap[4],int time){
-    int times[] = {0,0,0,0};
-    //curSteps = {0,0,0,0};
-    for(int i = 0; i < 4; i++){
-        wheelFreq[i] = velo*dirmap[i];
-        digitalWrite(MOTOR_DIR[i],wheelFreq[i]<0);
-        times[i] = 1000000/abs(wheelFreq[i]);
-    }
-    long start = millis();
-    while(start+time > millis()){
-        runSteppers(times);
-    }
-
     
+    if(running){
+        runSteppers();
+    }
+
 }
+
+
 
 void runSteppers(int times[]){
     for(int i = 0; i < 4; i++){
         digitalWrite(MOTOR_STEP[i],micros() % long(times[i]) > long(times[i]>>1));
     }
+}
+
+
+void onWire(int num){
+    int bFWD = Wire.read();
+    int bSIDE = Wire.read();
+    int bROT = Wire.read();
+    int bMULT = Wire.read();
+    //calc freq
+
+    running = false;
+    for(int i = 0; i < 4; i++){
+        wheelFreq[i] = FWD[i] * bFWD * bMULT;
+        wheelFreq[i] = SIDE[i] * bSIDE * bMULT;
+        wheelFreq[i] = ROT[i] * bROT * bMULT;
+        if(wheelFreq[i] != 0){
+            running = true;
+        }
+        digitalWrite(MOTOR_DIR[i],wheelFreq[i]<0);
+        times[i] = 1000000/abs(wheelFreq[i]);
+    }
+    digitalWrite(MOTOR_ENABLE,!running);
 }
