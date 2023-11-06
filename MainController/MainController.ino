2b9ca3203084026e9d920dbe051f30e783e24c38
LegionGjfter <a8956421@gmail.com>
2023-11-06 12:31:41 +0100

    WIP

--- MainController/MainController.ino
+++ MainController/MainController.ino
@@ -1,9 +1,9 @@
 #include <Wire.h>
 byte data[4] = {0,0,0,30};
 void setup() {
+  Wire.begin();
   Serial.begin(115200);
   Serial1.begin(115200);
-  Wire.begin();
 }
 
 void loop() {
@@ -18,3 +18,8 @@ void loop() {
   Serial.println("sending...");
   Serial1.println("Fuck You   0:"+String(data[0])+" 1:"+String(data[1])+" 2:"+String(data[2])+" 3:"+String(data[3]));
 }
+void loop() {
+	Wire.requestFrom(0x02, 1)
+	
+	while(Wire.available()){
+		char c=Wire.read();
